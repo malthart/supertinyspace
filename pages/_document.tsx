28362d4a36a1b2cd36acba09dfaa56e9f39fd4c9
Malthe Hartmann <malthart@icloud.com>
2023-08-01 18:21:30 +0200

    Meta added

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,16 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <title>
+          Malthe Alexius
+        </title>
+        <meta
+          name="description"
+          content="Built two banking experiences, a service marketplace & a boat insurance platform. Based in Copenhagen, Denmark."
+          key="desc"
+        />
+      </Head>
       <body className="bg-gray-50">
         <Main />
         <NextScript />

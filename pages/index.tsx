e89aa3b5f4e22d3c5a5c1b2b2c761697b3888719
Malthe Hartmann <malthart@icloud.com>
2023-08-18 11:02:38 +0200

    How I work

--- pages/index.tsx
+++ pages/index.tsx
@@ -211,10 +211,131 @@ export default function Home() {
             <a className="text-gray-500">Based in</a> Copenhagen, Denmark
           </h2>
         </div>
+
+        <Drawer.Root shouldScaleBackground>
+          <Drawer.Trigger asChild>
+            <button>How I work</button>
+          </Drawer.Trigger>
+          <Drawer.Portal>
+            <Drawer.Overlay className="fixed inset-0 bg-black/40" />
+            <Drawer.Content className="bg-white flex flex-col fixed bottom-0 left-0 right-0 h-[96%] rounded-t-[10px]">
+              <div className="max-w-md w-full mx-auto flex flex-col overflow-auto p-4 rounded-t-[10px]">
+                <div className="mx-auto w-12 h-1.5 flex-shrink-0 rounded-full bg-zinc-300 mb-8" />
+                <Drawer.Title className="font-medium mb-4">
+                  How I work
+                </Drawer.Title>
+                <p className="text-zinc-600 mb-2">
+                  In other words, this would be the answer to the question
+                  "What is your design process like?".
+                </p>
+                <p className="text-zinc-600 mb-2">
+                  Most people have a fluid process. And Im no exception.
+                  But, as an experienced designer, any time someone has
+                  asked about my process, I simply tell them I don&apos;t have
+                  a strict one: I instead have foundational design
+                  techniques and principles I adhere to and check my work
+                  against.
+                </p>
+                <p className="text-zinc-600 mb-2">
+                  "You must be shapeless, formless, like water". Being a
+                  Product Designer in todays
+                  world is no different. Each scenario is a different cup,
+                  and the designer must adhere to the circumstances of the
+                  cup — the cup is ever changing, like a company.
+                </p>
+                <p className="text-zinc-600 mb-6">
+                  I enjoy collaboration. I haven&apos;t always liked it: But now
+                  it&apos;s the first thing I cling too. It&apos;s a cheatcode into
+                  getting early-early buy-in and I mentor every designer
+                  to do the same. What stands in the way of collaboration
+                  and involvement? Ego. Fear of rejection and critism.
+                  Something that I have long left behind me.
+                </p>
+                <img className="mb-2" src="/IMG_3744 2.png"></img>
+                <p className="text-zinc-600 mb-6 text-xs">
+                  Rasmus, Lead of Growth, Thomas V, Lead of Consumer,
+                  Thomas, Lead of Squad Apollo, and me, jamming out in a
+                  live session in Copenhagen.
+                </p>
+                <p className="text-zinc-600 mb-2 font-medium">
+                  The Formalized Process
+                </p>
+                <p className="text-zinc-600 font-medium">
+                1. Discovery/Research/Play
+                </p>
+                <p className="text-zinc-600 mb-6">
+                I&apos;ll be spending time trying to uncover new problems,
+                opportunities, or helping someone defining a problem.
+                In this step its crucial for me to do loads of interviews
+                (both generic and contextual), while looking at NPS,
+                survey-data and competitor/ market analysis. All of this
+                material can be bundled and packed in decks to use on a
+                higher-level. Think: Director level. For me the more I
+                can send "upstream" the better. You might have noticed
+                the word &apos;Play&apos; in the header. What do you make of that?
+                For me, a workshop is the equivilent of Play: A place
+                where everyone can participate and the same level.
+                </p>
+                <img className="mb-2" src="/me.png"></img>
+                <p className="text-zinc-600 mb-6 text-xs">
+                  Me staring at the results of a &apos;Play&apos; session.
+                </p>
+                <p className="text-zinc-600 font-medium">
+                2. Prototype/Wireframe/Vizualize/Blueprint
+                </p>
+                <p className="text-zinc-600 mb-2">
+                What is our solution to the problem? We uncovered
+                in phase one? This is what we&apos;ll be debating here.
+                The best way to go about this step is to know what
+                is technically feasible. I use everything from
+                sketches, to animations, to documentation links,
+                to posts of Twitter shwoing something — everything
+                I can use to make my point as clear as day. That,
+                and then create something that I can test with a
+                user.
+                </p>
+                <p className="text-zinc-600 font-medium">
+                3. Test/Review
+                </p>
+                <p className="text-zinc-600 mb-2">
+                You could see this as a peer-review. But with
+                users. The solution we made in phase two? Time
+                to look the devil in the eye and find out if
+                its attractive, usable, lacking friendlyness or
+                juice. Maze, a usability tool, is amazing at this,
+                but sometimes, just sitting down next to someone
+                and hand them a phone with a prototype on it
+                can take you a long way.
+                </p>
+                <p className="text-zinc-600 font-medium">
+                4. Report/Reflect/Plan/Ideate
+                </p>
+                <p className="text-zinc-600 mb-2">
+                Coming soon
+                </p>
+                <p className="text-zinc-600 mb-2">
+                Coming soon
+                </p>
+                <p className="text-zinc-600 mb-2">
+                Coming soon
+                </p>
+                <p className="text-zinc-600 mb-2">
+                Coming soon
+                </p>
+              </div>
+            </Drawer.Content>
+          </Drawer.Portal>
+        </Drawer.Root>
+
+
+
+
       </div>
 
 
-    {/* <div className="py-8 px-8 grid grid-cols-2 md:grid-cols-4 gap-4">
+
+
+      {/* <div className="py-8 px-8 grid grid-cols-2 md:grid-cols-4 gap-4">
 
         <div className="grid gap-4">
           <div className="">

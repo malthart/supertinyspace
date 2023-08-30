eb11c795710033ce6fc7c4635afb5992959c3c8b
Malthe Hartmann <malthart@icloud.com>
2023-08-30 17:11:00 +0200

    Update _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,6 +4,9 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+      <title>
+          Malthe Alexius Hartmann
+        </title>
         <meta
           name="description"
           content="Built two banking experiences, a service marketplace & a boat insurance platform. Based in Copenhagen, Denmark."

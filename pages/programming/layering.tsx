d777377115fbc195c452708bac13465191e52cbf
Malthe Hartmann <malthart@icloud.com>
2023-10-20 23:38:05 +0200

    Changes

--- pages/programming/layering.tsx
+++ pages/programming/layering.tsx
@@ -1,6 +1,4 @@
 import { Inter } from 'next/font/google'
-import Link from 'next/link'
-import Image from 'next/image'
 import Layering from '@/components/layering'
 
 const inter = Inter({ subsets: ['latin'] })

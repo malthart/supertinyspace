a9e92a86d94cf6661b3d1c7aa6bb261b29a89f05
Malthe Hartmann <malthart@icloud.com>
2023-10-20 23:10:54 +0200

    Update layering.tsx

--- components/layering.tsx
+++ components/layering.tsx
@@ -7,7 +7,7 @@ const Layering = () => {
             <div className="relative h-[90vh] sm:h-[100vh] overflow-hidden transition-all duration-1000">
                 <img src="/programming/5.0db6da.webp" alt="" className="z-0 h-full w-full absolute object-cover top-0" />
                 <img src="/programming/4.1e07a4.webp" alt="" className="z-10 absolute object-contain -bottom-40 left-0 m-auto" />
-                <img src="/programming/3.9f3b94.webp" alt="" className="z-20 absolute object-contain bottom-20 sm:bottom-20 transition-all duration-1000 scale-110 sm:scale-100 lg:scale-100" />
+                <img src="/programming/3.9f3b94.webp" alt="" className="z-20 absolute object-contain bottom-10 sm:bottom-20 transition-all duration-1000 scale-110 sm:scale-100 lg:scale-100" />
                 <div className="z-30 absolute m-auto left-0 right-0 items-center flex flex-col justify-start pt-[12%]">
                     <div className="h-auto w-[250px] sm:w-[471px] transition-all duration-1000">
                         <img src="/programming/big-logo.dbe85a.png" alt="" className="h-[100%] w-[100%] block" />

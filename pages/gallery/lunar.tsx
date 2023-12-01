6956f10f580713a6344e7a3a88f0590eb35e1abd
Malthe Hartmann <malthart@icloud.com>
2023-12-01 16:17:02 +0100

    Commit.

--- pages/gallery/lunar.tsx
+++ pages/gallery/lunar.tsx
@@ -6,12 +6,12 @@ const inter = Inter({ subsets: ['latin'] })
 export default function Home() {
     return (
 
-        <main className="bg-stone-200 overflow-hidden w-full min-h-screen">
-            <div className="py-8 px-8 grid grid-cols-1 md:grid-cols-1 lg:grid-cols-3 place-items-left transition-all gap-6 sm:gap-20">
+        <main className="relative bg-stone-200 overflow-hidden w-full min-h-screen">
+            <div className="relative py-8 px-8 grid grid-cols-1 md:grid-cols-1 lg:grid-cols-3 transition-all gap-6 sm:gap-20">
 
                 <div className="items-top justify-left grid grid-cols-1">
                     <div>
-                        <div className="mb-24">
+                        <div className="mb-24 hover:bg-white/50 rounded-xl transition-all duration-300 w-fit p-2">
                             <Link className="text-stone-800 text-[18px] leading-[150%] flex items-center gap-0.25" href="/">
                                 <svg stroke="currentColor" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true" className="mr-2 rotate-180" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">
                                     <path fill-rule="evenodd" d="M16.28 11.47a.75.75 0 010 1.06l-7.5 7.5a.75.75 0 01-1.06-1.06L14.69 12 7.72 5.03a.75.75 0 011.06-1.06l7.5 7.5z" clip-rule="evenodd"></path>
@@ -25,17 +25,17 @@ export default function Home() {
                         <div className="text-stone-500 text-[18px] leading-[150%]">
                             <p>Product</p>
                             <p className="text-stone-800 mb-6">Lunar is a fintech company offering an alternative to traditional banking.
-                            It primarily targets millenials with a bank account through an app interface and an associated visa debit
-                            card. Lunar does not faciliate banking branches, making it a 100% digital experience. Lunar has several product
-                            lines in its belt, such as, but not limited to: Investing in stocks, ETF & crypto. Business banking. Loans.
-                            Buy Now Pay Later and a tiering system.</p>
+                                It primarily targets millenials with a bank account through an app interface and an associated visa debit
+                                card. Lunar does not faciliate banking branches, making it a 100% digital experience. Lunar has several product
+                                lines in its belt, such as, but not limited to: Investing in stocks, ETF & crypto. Business banking. Loans.
+                                Buy Now Pay Later and a tiering system.</p>
                         </div>
                         <div className="text-stone-500 text-[18px] leading-[150%]">
                             <p>Role</p>
                             <p className="text-stone-800 mb-6">Malthe was primarily tasked to lead discovery on the consumer vertical as
-                            well as business vertical. This ment speaking to users on a continous weekly basis, and creating early prototypes
-                            or desired products to test against users. This was helpful in reducing risk factors and negligence of new
-                            features and product releases.</p>
+                                well as business vertical. This ment speaking to users on a continous weekly basis, and creating early prototypes
+                                or desired products to test against users. This was helpful in reducing risk factors and negligence of new
+                                features and product releases.</p>
                         </div>
                         <div className="text-stone-500 text-[18px] leading-[150%]">
                             <p className="text-stone-800 mb-6">Projects he has been a part of and led:</p>
@@ -58,17 +58,90 @@ export default function Home() {
                     </div>
                 </div >
 
-                <div className="grid grid-cols-1 col-span-2 md:grid-cols-2 lg:grid-cols-2 place-items-left gap-4 transition-all">
-                    <img className="rounded-xl" src="/lunar/lunar_social_investing.webp" alt=""/>
-                    <img className="rounded-xl" src="/lunar/lunar_ipo_radar.webp" alt=""/>
-                    <img className="rounded-xl" src="/lunar/lunar_invest_news.webp" alt=""/>
-                    <img className="rounded-xl" src="/lunar/lunar_workshop.webp" alt=""/>
-                    <img className="rounded-xl" src="/lunar/lunar_thematics.webp" alt=""/>
-                    <img className="rounded-xl" src="/lunar/lunar_astralis.webp" alt=""/>
-                    <img className="rounded-xl" src="/lunar/lunar_risk_labels.webp" alt=""/>
-                    <img className="rounded-xl" src="/lunar/lunar_contextual_help.webp" alt=""/>
-                    <img className="rounded-xl" src="/lunar/lunar_share_stock.webp" alt=""/>
-                    <img className="rounded-xl" src="/lunar/lunar_ux.webp" alt=""/>
+                <div className="grid relative grid-cols-1 col-span-2 md:grid-cols-2 lg:grid-cols-2 place-items-left gap-4 transition-all">
+                    <div className="group relative appearance-none ">
+                        <div className="top-2 left-2 pointer-events-none absolute z-50 rounded-md bg-white/50 backdrop-blur-lg px-2 py-2 text-xs font-medium text-white transition-all duration-300">
+                            <svg className="fill-stone-200" xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24"><path d="M14 9l10 7.675-4.236.71 2.66 5.423-2.441 1.192-2.675-5.474-3.308 2.863v-12.389zm-7-7h5v-2h-5v2zm7 0h3v3h2v-5h-5v2zm-12 3v-3h3v-2h-5v5h2zm-2 7h2v-5h-2v5zm5 5h-3v-3h-2v5h5v-2zm12-10v1.781l2 1.535v-3.316h-2zm-10 12h5v-2h-5v2z" /></svg>
+                        </div>
+                        <img className="rounded-xl" src="/lunar/lunar_social_investing.webp" alt="" />
+                        <span className="bottom-2 left-2 right-2 object-center pointer-events-none absolute z-50 rounded-md bg-white/50 backdrop-blur-lg px-2 py-1 text-xs font-medium text-white opacity-0 group-hover:opacity-100 shadow-sm transition-all duration-300">
+                            I was tasked with creating a prototype showcasing aspects of social investing and test it against users.
+                        </span>
+                    </div>
+                    <div className="group relative appearance-none">
+                        <div className="top-2 left-2 pointer-events-none absolute z-50 rounded-md bg-white/50 backdrop-blur-lg px-2 py-2 text-xs font-medium text-white transition-all duration-300">
+                            <svg className="fill-stone-200" xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24"><path d="M14 9l10 7.675-4.236.71 2.66 5.423-2.441 1.192-2.675-5.474-3.308 2.863v-12.389zm-7-7h5v-2h-5v2zm7 0h3v3h2v-5h-5v2zm-12 3v-3h3v-2h-5v5h2zm-2 7h2v-5h-2v5zm5 5h-3v-3h-2v5h5v-2zm12-10v1.781l2 1.535v-3.316h-2zm-10 12h5v-2h-5v2z" /></svg>
+                        </div>
+                        <img className="rounded-xl" src="/lunar/lunar_ipo_radar.webp" alt="" />
+                        <span className="bottom-2 left-2 right-2 object-center pointer-events-none absolute z-50 rounded-md bg-white/50 backdrop-blur-lg px-2 py-1 text-xs font-medium text-white opacity-0 group-hover:opacity-100 shadow-sm transition-all duration-300">
+                            The IPO Radar came from "how might we levarage Lunar Invest above that of just being a place to buy and sell stock".
+                        </span>
+                    </div>
+                    <div className="group relative appearance-none">
+                        <div className="top-2 left-2 pointer-events-none absolute z-50 rounded-md bg-white/50 backdrop-blur-lg px-2 py-2 text-xs font-medium text-white transition-all duration-300">
+                            <svg className="fill-stone-200" xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24"><path d="M14 9l10 7.675-4.236.71 2.66 5.423-2.441 1.192-2.675-5.474-3.308 2.863v-12.389zm-7-7h5v-2h-5v2zm7 0h3v3h2v-5h-5v2zm-12 3v-3h3v-2h-5v5h2zm-2 7h2v-5h-2v5zm5 5h-3v-3h-2v5h5v-2zm12-10v1.781l2 1.535v-3.316h-2zm-10 12h5v-2h-5v2z" /></svg>
+                        </div>
+                        <img className="rounded-xl" src="/lunar/lunar_invest_news.webp" alt="" />
+                        <span className="bottom-2 left-2 right-2 object-center pointer-events-none absolute z-50 rounded-md bg-white/50 backdrop-blur-lg px-2 py-1 text-xs font-medium text-white opacity-0 group-hover:opacity-100 shadow-sm transition-all duration-300">
+                            Through my research with users, it became clear that the majority was seeking information outside of Lunar.
+                        </span>
+                    </div>
+                    <img className="rounded-xl" src="/lunar/lunar_workshop.webp" alt="" />
+                    <div className="group relative appearance-none">
+                        <div className="top-2 left-2 pointer-events-none absolute z-50 rounded-md bg-white/50 backdrop-blur-lg px-2 py-2 text-xs font-medium text-white transition-all duration-300">
+                            <svg className="fill-stone-200" xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24"><path d="M14 9l10 7.675-4.236.71 2.66 5.423-2.441 1.192-2.675-5.474-3.308 2.863v-12.389zm-7-7h5v-2h-5v2zm7 0h3v3h2v-5h-5v2zm-12 3v-3h3v-2h-5v5h2zm-2 7h2v-5h-2v5zm5 5h-3v-3h-2v5h5v-2zm12-10v1.781l2 1.535v-3.316h-2zm-10 12h5v-2h-5v2z" /></svg>
+                        </div>
+                        <img className="rounded-xl" src="/lunar/lunar_thematics.webp" alt="" />
+                        <span className="bottom-2 left-2 right-2 object-center pointer-events-none absolute z-50 rounded-md bg-white/50 backdrop-blur-lg px-2 py-1 text-xs font-medium text-white opacity-0 group-hover:opacity-100 shadow-sm transition-all duration-300">
+                            I created thematic investing based of off my research with beginner investors looking for more than numbers; they wanted purpose.
+                        </span>
+                    </div>
+                    <div className="group relative appearance-none">
+                        <div className="top-2 left-2 pointer-events-none absolute z-50 rounded-md bg-white/50 backdrop-blur-lg px-2 py-2 text-xs font-medium text-white transition-all duration-300">
+                            <svg className="fill-stone-200" xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24"><path d="M14 9l10 7.675-4.236.71 2.66 5.423-2.441 1.192-2.675-5.474-3.308 2.863v-12.389zm-7-7h5v-2h-5v2zm7 0h3v3h2v-5h-5v2zm-12 3v-3h3v-2h-5v5h2zm-2 7h2v-5h-2v5zm5 5h-3v-3h-2v5h5v-2zm12-10v1.781l2 1.535v-3.316h-2zm-10 12h5v-2h-5v2z" /></svg>
+                        </div>
+                        <img className="rounded-xl" src="/lunar/lunar_astralis.webp" alt="" />
+                        <span className="bottom-2 left-2 right-2 object-center pointer-events-none absolute z-50 rounded-md bg-white/50 backdrop-blur-lg px-2 py-1 text-xs font-medium text-white opacity-0 group-hover:opacity-100 shadow-sm transition-all duration-300">
+                            Acted as the Assitant Facilitaror (Co-Facilitator) to the Facilitator of the Design Sprint for the Astralis & Lunar product collaboration.
+                        </span>
+                    </div>
+                    <div className="group relative appearance-none">
+                        <div className="top-2 left-2 pointer-events-none absolute z-50 rounded-md bg-white/50 backdrop-blur-lg px-2 py-2 text-xs font-medium text-white transition-all duration-300">
+                            <svg className="fill-stone-200" xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24"><path d="M14 9l10 7.675-4.236.71 2.66 5.423-2.441 1.192-2.675-5.474-3.308 2.863v-12.389zm-7-7h5v-2h-5v2zm7 0h3v3h2v-5h-5v2zm-12 3v-3h3v-2h-5v5h2zm-2 7h2v-5h-2v5zm5 5h-3v-3h-2v5h5v-2zm12-10v1.781l2 1.535v-3.316h-2zm-10 12h5v-2h-5v2z" /></svg>
+                        </div>
+                        <img className="rounded-xl" src="/lunar/lunar_risk_labels.webp" alt="" />
+                        <span className="bottom-2 left-2 right-2 object-center pointer-events-none absolute z-50 rounded-md bg-white/50 backdrop-blur-lg px-2 py-1 text-xs font-medium text-white opacity-0 group-hover:opacity-100 shadow-sm transition-all duration-300">
+                            Explored risk labels and other dynamic messages to inform and educate users on investment concepts.
+                        </span>
+                    </div>
+                    <div className="group relative appearance-none">
+                        <div className="top-2 left-2 pointer-events-none absolute z-50 rounded-md bg-white/50 backdrop-blur-lg px-2 py-2 text-xs font-medium text-white transition-all duration-300">
+                            <svg className="fill-stone-200" xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24"><path d="M14 9l10 7.675-4.236.71 2.66 5.423-2.441 1.192-2.675-5.474-3.308 2.863v-12.389zm-7-7h5v-2h-5v2zm7 0h3v3h2v-5h-5v2zm-12 3v-3h3v-2h-5v5h2zm-2 7h2v-5h-2v5zm5 5h-3v-3h-2v5h5v-2zm12-10v1.781l2 1.535v-3.316h-2zm-10 12h5v-2h-5v2z" /></svg>
+                        </div>
+                        <img className="rounded-xl" src="/lunar/lunar_contextual_help.webp" alt="" />
+                        <span className="bottom-2 left-2 right-2 object-center pointer-events-none absolute z-50 rounded-md bg-white/50 backdrop-blur-lg px-2 py-1 text-xs font-medium text-white opacity-0 group-hover:opacity-100 shadow-sm transition-all duration-300">
+                            Contextual help sought out to create a layer of information and answer questions that would otherwise be directed to support.
+                        </span>
+                    </div>
+                    <div className="group relative appearance-none">
+                        <div className="top-2 left-2 pointer-events-none absolute z-50 rounded-md bg-white/50 backdrop-blur-lg px-2 py-2 text-xs font-medium text-white transition-all duration-300">
+                            <svg className="fill-stone-200" xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24"><path d="M14 9l10 7.675-4.236.71 2.66 5.423-2.441 1.192-2.675-5.474-3.308 2.863v-12.389zm-7-7h5v-2h-5v2zm7 0h3v3h2v-5h-5v2zm-12 3v-3h3v-2h-5v5h2zm-2 7h2v-5h-2v5zm5 5h-3v-3h-2v5h5v-2zm12-10v1.781l2 1.535v-3.316h-2zm-10 12h5v-2h-5v2z" /></svg>
+                        </div>
+                        <img className="rounded-xl transition-all duration-300" src="/lunar/lunar_share_stock.webp" alt="" />
+                        <span className="bottom-2 left-2 right-2 object-center pointer-events-none absolute z-50 rounded-md bg-white/50 backdrop-blur-lg px-2 py-1 text-xs font-medium text-white opacity-0 group-hover:opacity-100 shadow-sm transition-all duration-300">
+                            An idea born from the social investing project. Users could share links to a stock overview from the stock page and assist Lunar in user acquisition.
+                        </span>
+                    </div>
+                    <img className="rounded-xl" src="/lunar/lunar_ux.webp" alt="" />
+                </div>
+
+                <div className="flex flex-row left-5 bottom-5 fixed gap-2">
+                    <span className="bg-black/50 pointer-events-none rounded-md backdrop-blur-lg px-2 py-2 text-xs font-medium text-white transition-all duration-300">
+                        Hover/Tap images with the icon:
+                    </span>
+                    <div className="bg-black/50 backdrop-blur-lg  pointer-events-none z-50 rounded-md px-2 py-2 text-xs font-medium text-white transition-all duration-300">
+                        <svg className="fill-stone-200" xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24"><path d="M14 9l10 7.675-4.236.71 2.66 5.423-2.441 1.192-2.675-5.474-3.308 2.863v-12.389zm-7-7h5v-2h-5v2zm7 0h3v3h2v-5h-5v2zm-12 3v-3h3v-2h-5v5h2zm-2 7h2v-5h-2v5zm5 5h-3v-3h-2v5h5v-2zm12-10v1.781l2 1.535v-3.316h-2zm-10 12h5v-2h-5v2z" /></svg>
+                    </div>
                 </div>
 
             </div>
--- pages/index.tsx
+++ pages/index.tsx
@@ -26,14 +26,14 @@ export default function Home() {
                                     </div>
                                     <div>
                                         <h1 className="text-stone-500 text-[18px] leading-[150%]">Malthe Hartmann</h1>
-                                        <p>Product & Research.</p>
+                                        <p>Product & Research</p>
                                     </div>
                                 </div>
                                 <div className="my-auto flex flex-row gap-2">
-                                    <Link href="https://twitter.com/malthart" target='_blank'>
+                                    <Link className="active:scale-90 transition-all duration-200" href="https://twitter.com/malthart" target='_blank'>
                                         <Image src="/twitter.3.ico" alt="" width={20} height={20} className="rounded" />
                                     </Link>
-                                    <Link href="mailto:malthart@icloud.com" target='_blank'>
+                                    <Link className="active:scale-90 transition-all duration-200" href="mailto:malthart@icloud.com" target='_blank'>
                                         <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path d="M13.718 10.528c0 .792-.268 1.829-.684 2.642-1.009 1.98-3.063 1.967-3.063-.14 0-.786.27-1.799.687-2.58 1.021-1.925 3.06-1.624 3.06.078zm10.282 1.472c0 6.627-5.373 12-12 12s-12-5.373-12-12 5.373-12 12-12 12 5.373 12 12zm-5-1.194c0-3.246-2.631-5.601-6.256-5.601-4.967 0-7.744 3.149-7.744 7.073 0 3.672 2.467 6.517 7.024 6.517 2.52 0 4.124-.726 5.122-1.288l-.687-.991c-1.022.593-2.251 1.136-4.256 1.136-3.429 0-5.733-2.199-5.733-5.473 0-5.714 6.401-6.758 9.214-5.071 2.624 1.642 2.524 5.578.582 7.083-1.034.826-2.199.799-1.821-.756 0 0 1.212-4.489 1.354-4.975h-1.364l-.271.952c-.278-.785-.943-1.295-1.911-1.295-2.018 0-3.722 2.19-3.722 4.783 0 1.73.913 2.804 2.38 2.804 1.283 0 1.95-.726 2.364-1.373-.3 2.898 5.725 1.557 5.725-3.525z" /></svg>
                                     </Link>
                                 </div>
@@ -41,7 +41,7 @@ export default function Home() {
                             <div className="">
                                 <p className="text-stone-800 leading-[150%] text-base">Malthe Alexius Hartmann
                                     is a strategic Product Designer with a focus on user research. He has abilities
-                                    in discoverying problems and turning those problem into business opportunities.
+                                    in discovering problems and turning those problem into business opportunities.
                                     Avid triathlete.</p>
                             </div>
                         </div>
@@ -102,6 +102,10 @@ export default function Home() {
                                     <Link className="font-medium hover:underline transition-all duration-500" href="https://www.linkedin.com/company/rise-card/">Website & App</Link>
                                 </div>
                                 <div className="flex flex-col">
+                                    <p className="text-sm">Parea</p>
+                                    <Link className="font-medium hover:underline transition-all duration-500" href="https://www.linkedin.com/company/rise-card/">App</Link>
+                                </div>
+                                <div className="flex flex-col">
                                     <p className="text-sm">Shouter</p>
                                     <Link className="font-medium hover:underline transition-all duration-500" href="https://shouter.app/">Web Platform</Link>
                                 </div>
@@ -131,9 +135,18 @@ export default function Home() {
                         </div>
 
                         <div className="grid grid-cols-1 col-span-2 sm:grid-cols-2 place-items-left gap-4 transition-all mb-12">
-                            <Link href="/gallery/parea"><img className="rounded-xl" src="/parea/parea_map.webp" alt="" /></Link>
-                            <Link href="/gallery/rise"><img className="rounded-xl" src="/rise/rise_spending.webp" alt="" /></Link>
-                            <Link href="/gallery/lunar"><img className="rounded-xl" src="/lunar/lunar_social_investing.webp" alt="" /></Link>
+                            <Link className="group relative appearance-none" href="/gallery/parea">
+                                <img className="rounded-xl z-10" src="/parea/parea_map.webp" alt="" />
+                                <span className="bottom-2 left-2 object-center pointer-events-none absolute z-50 w-max rounded-md bg-white/50 backdrop-blur-lg px-2 py-1 text-xs font-semibold text-white opacity-0 group-hover:opacity-100 shadow-sm"> Parea </span>
+                            </Link>
+                            <Link className="group relative appearance-none" href="/gallery/rise">
+                                <img className="rounded-xl" src="/rise/rise_spending.webp" alt="" />
+                                <span className="bottom-2 left-2 object-center pointer-events-none absolute z-50 w-max rounded-md bg-white/50 backdrop-blur-lg px-2 py-1 text-xs font-semibold text-white opacity-0 group-hover:opacity-100 shadow-sm"> RISE </span>
+                            </Link>
+                            <Link className="group relative appearance-none" href="/gallery/lunar">
+                                <img className="rounded-xl" src="/lunar/lunar_social_investing.webp" alt="" />
+                                <span className="bottom-2 left-2 object-center pointer-events-none absolute z-50 w-max rounded-md bg-white/50 backdrop-blur-lg px-2 py-1 text-xs font-semibold text-white opacity-0 group-hover:opacity-100 shadow-sm"> Lunar </span>
+                            </Link>
                         </div >
 
                         {/* <div className="flex">

5697ff367c8ab12783fc6a6f1bb994157f03f7e8
Malthe Hartmann <malthart@icloud.com>
2024-01-26 17:11:48 +0100

    prgorm

--- pages/programming/menudrawer/menudrawer.tsx
+++ pages/programming/menudrawer/menudrawer.tsx
@@ -10,24 +10,29 @@ export default function MenuDrawer() {
                 <div className="grid h-full max-w-lg grid-cols-4 mx-auto font-medium">
                     <Drawer.Trigger asChild>
                         <button type="button" className="inline-flex flex-col items-center justify-center px-5 hover:bg-gray-50 dark:hover:bg-gray-800 group">
-                            <svg className="w-5 h-5 text-gray-500 dark:text-gray-400 group-hover:text-blue-600 dark:group-hover:text-blue-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-                                <path d="m19.707 9.293-2-2-7-7a1 1 0 0 0-1.414 0l-7 7-2 2a1 1 0 0 0 1.414 1.414L2 10.414V18a2 2 0 0 0 2 2h3a1 1 0 0 0 1-1v-4a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v4a1 1 0 0 0 1 1h3a2 2 0 0 0 2-2v-7.586l.293.293a1 1 0 0 0 1.414-1.414Z" />
-                            </svg>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="lucide lucide-search"><circle cx="11" cy="11" r="8" /><path d="m21 21-4.3-4.3" /></svg>
                         </button>
                     </Drawer.Trigger>
                 </div>
             </div>
             <Drawer.Portal>
                 <Drawer.Overlay className="fixed inset-0 bg-black/40 backdrop-blur-sm" />
-                <Drawer.Content className="bg-zinc-100 flex flex-col rounded-t-[10px] mt-24 fixed min-h-[250px] bottom-0 left-0 right-0">
+                <Drawer.Content className="bg-zinc-100 flex flex-col rounded-t-[10px] mt-24 fixed max-h-[25%] bottom-0 left-0 right-0">
                     <div className="p-4 bg-white rounded-t-[4px] flex-1">
                         <div className="max-w-md mx-auto">
                             <Drawer.Title className="font-medium">
-                                New Thread
+                                What are you looking for?
                             </Drawer.Title>
                             <p className="text-zinc-600 text-sm">
-                                ChatGPT 4
+                                Search for anything and everything
                             </p>
+                            <textarea
+                                className="border border-gray-400/25 mt-2 w-full rounded p-2 mb-1"
+                                placeholder="What is 2 x 2?"
+                            />
+                            <button className="bg-blue-500 hover:bg-blue-700 text-white font-medium py-2 px-4 rounded w-full">
+                                Go look
+                            </button>
                         </div>
                     </div>
                 </Drawer.Content>
